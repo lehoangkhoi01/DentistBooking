8f929de8ee1c7935256be9483e7a5dbf1d41a06d
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2022-06-05 16:13:25 +0700

    Add Service repository

---
+++ DentistBooking/DataAccess/Repository/ServiceRepository.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObject;
+using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        public void AddNewService(Service service) => ServiceDAO.Instance.AddNewService(service);
+
+        public Service GetServiceById(int id) => ServiceDAO.Instance.GetServiceById(id);
+
+        public IEnumerable<Service> GetServiceList() => ServiceDAO.Instance.GetServiceList();
+
+        public void UpdateService(Service service) => ServiceDAO.Instance.UpdateService(service);
+    }
+}
---
+++ DentistBooking/DataAccess/ServiceDAO.cs
@@ -0,0 +1,91 @@
+﻿using BusinessObject;
+using BusinessObject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ServiceDAO
+    {
+        private static ServiceDAO instance = null;
+        private static readonly object instanceLock = new object();
+
+        private ServiceDAO() { }
+        public static ServiceDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ServiceDAO();
+                    }
+                }
+                return instance;
+            }
+        }
+        //-----------------------------------------------------------------
+
+        public void AddNewService(Service service)
+        {
+            try
+            {
+                var dbContext = new DentistBookingContext();
+                dbContext.Services.AddAsync(service);
+                dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Service GetServiceById(int id)
+        {
+            Service service;
+            try
+            {
+                var dbContext = new DentistBookingContext();
+                service = dbContext.Services.FirstOrDefault(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return service;
+        }
+
+        public void UpdateService(Service service)
+        {
+            try
+            {
+                var dbContext = new DentistBookingContext();
+                dbContext.Services.Update(service);
+                dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IEnumerable<Service> GetServiceList()
+        {
+            IEnumerable<Service> serviceList;
+            try
+            {
+                var dbContext = new DentistBookingContext();
+                serviceList = dbContext.Services.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return serviceList;
+        }
+    }
+}
