59239eb215ceb24d684f8a85baa5d11054a2d49f
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2022-06-21 19:48:23 +0700

    Add Reservation Repository

---
+++ DentistBooking/DataAccess/Interfaces/IReservationRepository.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IReservationRepository
+    {
+        public Reservation GetReservationById(int id);
+        public IEnumerable<Reservation> GetReservations();
+        public IEnumerable<Reservation> GetReservationsByCustomerId(int customerId);
+        public IEnumerable<Reservation> GetReservationsByDentistId(int dentistId);
+        public IEnumerable<Reservation> GetReservationsByServiceId(int serviceId);
+        public void AddNewReservation(Reservation reservation);
+        public void UpdateReservation(Reservation reservation);
+    }
+}
