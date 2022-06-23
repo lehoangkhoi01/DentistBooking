9147f56a3872a268403642af2debb30a88735473
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2022-06-23 21:18:43 +0700

    Add function for customer to make reservation

--- DentistBooking/DentistBookingWebApp/Pages/Reservation/Index.cshtml.cs
+++ DentistBooking/DentistBookingWebApp/Pages/Reservation/Index.cshtml.cs
@@ -17,14 +17,20 @@ namespace DentistBookingWebApp.Pages.Reservation
         private readonly IServiceRepository serviceRepository;
         private readonly IDentistRepository dentistRepository;
         private readonly IReservationRepository reservationRepository;
+        private readonly ICustomerRepository customerRepository;
+        private readonly IUserRepository userRepository;
 
-        public IndexModel(IServiceRepository serviceRepository, 
+        public IndexModel(IServiceRepository serviceRepository,
                             IDentistRepository dentistRepository,
-                            IReservationRepository reservationRepository)
+                            IReservationRepository reservationRepository,
+                            ICustomerRepository customerRepository,
+                            IUserRepository userRepository)
         {
             this.serviceRepository = serviceRepository;
             this.dentistRepository = dentistRepository;
             this.reservationRepository = reservationRepository;
+            this.customerRepository = customerRepository;
+            this.userRepository = userRepository;
         }
 
         public IList<string> TimeList { get; set; }
@@ -32,11 +38,11 @@ namespace DentistBookingWebApp.Pages.Reservation
         public IActionResult OnGet()
         {
             string roleId = HttpContext.Session.GetString("ROLE");
-            if(string.IsNullOrEmpty(roleId))
+            if (string.IsNullOrEmpty(roleId))
             {
                 return RedirectToPage("/Login");
             }
-            else if(roleId != "2")
+            else if (roleId != "2")
             {
                 return NotFound();
             }
@@ -47,7 +53,7 @@ namespace DentistBookingWebApp.Pages.Reservation
             {
                 IEnumerable<Service> services = serviceRepository.GetServiceList();
                 IEnumerable<Dentist> dentists = dentistRepository.GetDentistList();
-                
+
                 ViewData["Service"] = new SelectList(services, "Id", "Name");
                 ViewData["DentistList"] = new SelectList(dentists, "Id", "FullName");
                 ViewData["TimeList"] = new SelectList(TIME_LIST);
@@ -60,6 +66,64 @@ namespace DentistBookingWebApp.Pages.Reservation
             return Page();
         }
 
+        public IActionResult OnPostMakeReservation([FromForm] string date, string time, int serviceId, int dentistId)
+        {
+            string email = HttpContext.Session.GetString("EMAIL");
+            if (string.IsNullOrEmpty(email))
+            {
+                return RedirectToPage("/Login");
+            }
+
+            if (string.IsNullOrEmpty(date) || string.IsNullOrEmpty(time))
+            {
+                TempData["ErrorMessage"] = "You must choose date and time to make reservation";
+                return Page();
+            }
+
+            try
+            {
+                var dateTimeString = date + " " + time;
+                DateTime dateTime = DateTime.ParseExact(dateTimeString, "dd-MM-yyyy HH:mm", CultureInfo.CurrentCulture);
+                User user = userRepository.GetUserByEmail(email);
+                Customer customer = customerRepository.GetCustomerByUserId(user.Id);
+                Service service = serviceRepository.GetServiceById(serviceId);
+                if(service == null)
+                {
+                    return NotFound();
+                }
+
+                // Auto choose random dentist if user not demand specific
+                if(dentistId == 0)
+                {
+                    IEnumerable<Dentist> availableDentists = GetAvailableDentist(dateTime);
+                    int indexRandom = new Random().Next(availableDentists.Count());
+                    Dentist randomDentist = availableDentists.ElementAt(indexRandom);
+                    dentistId = randomDentist.Id;
+                }
+
+                BusinessObject.Reservation reservation = new BusinessObject.Reservation
+                {
+                    CustomerId = customer.Id,
+                    DentistId = dentistId,
+                    Price = service.Price,
+                    ServiceId = serviceId,
+                    CreatedDate = DateTime.Now,
+                    UpdatedDate = DateTime.Now,
+                    ResevrationDate = dateTime,
+                    Status = "Waiting",
+                };
+
+                reservationRepository.AddNewReservation(reservation);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+
         public IActionResult OnPostLoadDentist([FromForm] string date, string time)
         {
             IEnumerable<Dentist> dentists;
@@ -67,24 +131,31 @@ namespace DentistBookingWebApp.Pages.Reservation
             try
             {
                 services = serviceRepository.GetServiceList();
-                dentists = dentistRepository.GetDentistList();
+                //dentists = dentistRepository.GetDentistList();
 
                 if (!string.IsNullOrEmpty(date) && !string.IsNullOrEmpty(time))
                 {
                     var dateTimeString = date + " " + time;
                     DateTime dateTime = DateTime.ParseExact(dateTimeString, "dd-MM-yyyy HH:mm", CultureInfo.CurrentCulture);
-                    IList<Dentist> busyDentists = new List<Dentist>().ToList();
-                    IEnumerable<BusinessObject.Reservation> reservations = reservationRepository.GetReservationsByDateTime(dateTime);                                     
-                    if (reservations.Count() > 0)
-                    {
-                        foreach (var item in reservations)
-                        {
-                            busyDentists.Add(item.Dentist);
-                        }
-                        dentists = dentists.Where(i => !busyDentists.Contains(i));
-                    }
+                    dentists = GetAvailableDentist(dateTime);
+
+                    //IList<Dentist> busyDentists = new List<Dentist>().ToList();
+                    //IEnumerable<BusinessObject.Reservation> reservations = reservationRepository.GetReservationsByDateTime(dateTime);
+                    //if (reservations.Count() > 0)
+                    //{
+                    //    foreach (var item in reservations)
+                    //    {
+                    //        busyDentists.Add(item.Dentist);
+                    //    }
+                    //    dentists = dentists.Where(i => !busyDentists.Contains(i));
+                    //}
                     //ViewData["DentistList"] = new SelectList(dentists, "Id", "FullName");
                 }
+                else
+                {
+                    dentists = null;
+                }
+
                 ViewData["DentistList"] = new SelectList(dentists, "Id", "FullName");
                 ViewData["Service"] = new SelectList(services, "Id", "Name");
                 ViewData["TimeList"] = new SelectList(TIME_LIST, time);
@@ -95,8 +166,32 @@ namespace DentistBookingWebApp.Pages.Reservation
                 TempData["ErrorMessage"] = ex.Message;
             }
 
-            
             return Page();
+        }
+
+        private IEnumerable<Dentist> GetAvailableDentist(DateTime dateTime)
+        {
+            IEnumerable<Dentist> dentists;
+            try
+            {
+                dentists = dentistRepository.GetDentistList();
+                IList<Dentist> busyDentists = new List<Dentist>().ToList();
+
+                IEnumerable<BusinessObject.Reservation> reservations = reservationRepository.GetReservationsByDateTime(dateTime);
+                if (reservations.Count() > 0)
+                {
+                    foreach (var item in reservations)
+                    {
+                        busyDentists.Add(item.Dentist);
+                    }
+                    dentists = dentists.Where(i => !busyDentists.Contains(i));
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return dentists;
         }
     }
 }
