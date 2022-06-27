03ca6d2acb56bf8be0bb28f4bbbd33c0f705554b
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2022-06-27 19:49:50 +0700

    Update UI for reservation page

--- DentistBooking/DataAccess/UserDAO.cs
+++ DentistBooking/DataAccess/UserDAO.cs
@@ -76,7 +76,7 @@ namespace DataAccess
             try
             {
                 var dbContext = new DentistBookingContext();
-                user = dbContext.Users.Include(u => u.Role.Name).FirstOrDefault(u => u.Id == id);
+                user = dbContext.Users.Include(u => u.Role).FirstOrDefault(u => u.Id == id);
             }
             catch (Exception ex)
             {
